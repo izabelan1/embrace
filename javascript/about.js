7fe3101c31d1505bf8a16201cfd71fda92e079f8
izabelan1 <Izabela.nair@yahoo.co.uk>
2023-05-29 12:41:55 +0100

    Add files via upload

---
+++ javascript/about.js
@@ -0,0 +1,3 @@
+function shop() {
+    alert("Shop is under maintanance. Come back soon!");
+}
---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+function submit() {
+    // alert("function clocked")
+    // window.location.href = ".";
+    window.location.href = "submit.html";
+}
+
+function comingsoon() {
+    alert("Story will be published shortly. Please come back!");
+}
